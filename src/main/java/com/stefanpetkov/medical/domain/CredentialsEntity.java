20ea3eb95b0ae8de748c9470756b0d5ade54ff48
Stefan Petkov <spp.bg@abv.bg>
2023-01-05 10:43:31 +0200

    registration form validation and entity email validation

--- src/main/java/com/stefanpetkov/medical/domain/CredentialsEntity.java
+++ src/main/java/com/stefanpetkov/medical/domain/CredentialsEntity.java
@@ -5,6 +5,8 @@ package com.stefanpetkov.medical.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,8 @@ public class CredentialsEntity {
     private Long credentialsId;
 
     @Column(length = 150, nullable = false, unique = true)
+    @Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    @NotEmpty(message = "Email cannot be empty")
     private String email;
 
     @Column(length = 150, nullable = false, unique = true)
@@ -27,7 +31,7 @@ public class CredentialsEntity {
     @OneToOne(cascade = CascadeType.ALL)
     private PatientEntity patient;
 
-   @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.ALL)
     private DoctorEntity doctor;
 
     /*
