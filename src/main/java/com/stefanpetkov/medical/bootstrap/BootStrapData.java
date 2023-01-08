2208d8336d935f20b34329070852c61c783c83bb
Georgi Dinov <georgidinov1@abv.bg>
2023-01-08 15:46:04 +0200

    develop/security: intermediate commit - minor refactoring

--- src/main/java/com/stefanpetkov/medical/bootstrap/BootStrapData.java
+++ src/main/java/com/stefanpetkov/medical/bootstrap/BootStrapData.java
@@ -8,9 +8,12 @@ import com.stefanpetkov.medical.repositories.AppointmentRepository;
 import com.stefanpetkov.medical.repositories.CredentialsRepository;
 import com.stefanpetkov.medical.repositories.DoctorRepository;
 import com.stefanpetkov.medical.repositories.PatientRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+
+@Slf4j
 @Component
 public class BootStrapData implements CommandLineRunner {
 
@@ -41,49 +44,40 @@ public class BootStrapData implements CommandLineRunner {
         doctor.setFirstName("Ivan");
         doctor.setLastName("Ivanov");
         doctor.setPhone("08987777777");
-        doctor.getFirstName();
-        doctor.getLastName();
-        doctor.getPhone();
         doctorRepository.save(doctor);
 
-        System.out.println("Doctors");
-        System.out.println("number of doctors " +  doctorRepository.count());
+        log.info("Doctors");
+        log.info("Number of doctors = {}", doctorRepository.count());
 
         PatientEntity patient = new PatientEntity();
         patient.setFirstName("Stefan");
         patient.setLastName("Stefanov");
         patient.setPhone("0893343333");
         patient.setComment("Any comments!!!");
-        patient.getFirstName();
-        patient.getLastName();
-        patient.getPhone();
-        patient.getComment();
         patientRepository.save(patient);
 
-        System.out.println("patient");
-        System.out.println("number of patient " +  patientRepository.count());
+        log.info("Patients");
+        log.info("Number of patients = {}", patientRepository.count());
 
 
         CredentialsEntity credentials = new CredentialsEntity();
         credentials.setEmail("spp1.bg@abv.bg");
         credentials.setPassword("pass");
         credentials.setCredentialsId(5L);
-        credentials.getPatient();
-        credentials.getEmail();
         credentialsRepository.save(credentials);
-        System.out.println("credential");
-        System.out.println("number of credential " +  credentialsRepository.count());
+
+        log.info("Credentials");
+        log.info("Number of credential  = {}", credentialsRepository.count());
 
 
         AppointmentEntity appointment = new AppointmentEntity();
         appointment.setAppointment("03.01.2023");
         //appointment.setPatient(patientRepository.findPatientByPatientId(1L));
-        appointment.getAppointment();
-       // appointment.getPatient();
+        // appointment.getPatient();
         appointmentRepository.save(appointment);
 
-        System.out.println("appointment");
-        System.out.println("number of appointments " +  appointmentRepository.count());
+        log.info("Appointments");
+        log.info("Number of appointments = {}", appointmentRepository.count());
     }
 
 
