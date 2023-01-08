c1ff0207ce4dc699ef20d9b10090775cb4eedd25
Georgi Dinov <georgidinov1@abv.bg>
2023-01-08 13:58:12 +0200

    develop/georgi: rough refactoring

--- src/main/java/com/stefanpetkov/medical/controllers/AuthenticationController.java
+++ src/main/java/com/stefanpetkov/medical/controllers/AuthenticationController.java
@@ -3,19 +3,19 @@ package com.stefanpetkov.medical.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class AuthenticationController {
 
 
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String login(Model model) {
         return "login/login.html";
     }
 
-    @RequestMapping("/register")
+    @GetMapping("/register")
     public String register() {
         return "register/register.html";
     }
