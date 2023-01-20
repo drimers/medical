de6e0ca9e0c7f5f07c61a97557e1c72dcf915326
Stefan Petkov <spp.bg@abv.bg>
2023-01-20 14:37:24 +0200

    add mapping logon

--- src/main/java/com/stefanpetkov/medical/controllers/CredentialsController.java
+++ src/main/java/com/stefanpetkov/medical/controllers/CredentialsController.java
@@ -2,21 +2,27 @@ package com.stefanpetkov.medical.controllers;
 
 
 import com.stefanpetkov.medical.commands.PatientCommand;
+import com.stefanpetkov.medical.domain.Appointment;
+import com.stefanpetkov.medical.domain.Doctor;
 import com.stefanpetkov.medical.domain.UserCredentials;
 import com.stefanpetkov.medical.repositories.CredentialsRepository;
+import com.stefanpetkov.medical.services.AppointmentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class CredentialsController {
 
-    private final CredentialsRepository credentialsRepository;
 
+    private final AppointmentService appointmentService;
 
     @GetMapping("/login")
     public String showLoginForm(Model model) {
@@ -32,6 +38,14 @@ public class CredentialsController {
         return "register";
     }
 
+    @RequestMapping(value = "/logon", method = RequestMethod.POST)
+    public String login(@ModelAttribute("credential") UserCredentials credentials,
+                        @ModelAttribute("credential") Appointment appointment,
+                        Model model) {
+        model.addAttribute("appointments", appointmentService.appointmentsForPatient(2L) );
+        // TODO: not implemented spring security
+        return "patient/patientAppointments";
+    }
 
 }
 
